544978037790ea08748b3f04c2a52230f02f905d
Bala <balaarjunan@gmail.com>
2016-05-04 18:03:31 -0700

    Added condition for hidden element check

--- testUtillity.js
+++ testUtillity.js
@@ -2,31 +2,51 @@
     console.clear();
     var elemToTest = prompt("Enter the element to test and it's property - (Comma Seperated as given below)", "textbox,border");
     if (elemToTest != null) {
-        var inputField = false;
 
         var elemType = elemToTest.split(',')[0];
         var propertyType = elemToTest.split(',')[1];
-
+        var listOfElements = [];
         switch (elemType) {
             case 'textbox':
                 var elems = document.body.getElementsByTagName("input");
-                inputField = true;
+                // Filter text boxes
+                for (var k = 0; k < elems.length; k++) {
+                    if(elems[k].type == "text"){
+                        listOfElements.push(elems[k])
+                    }
+                }
                 break;
 
             case 'button':
-                var elems = document.body.getElementsByTagName("button");
+                var elems = document.body.getElementsByTagName("input");
+                // Filter input buttons
+                for (var k = 0; k < elems.length; k++) {
+                    if(elems[k].type == "button" || elems[k].type == "submit"){
+                        listOfElements.push(elems[k])
+                    }
+                }
+                var buttonElems= document.body.getElementsByTagName("button");
+                // Filter input buttons
+                for (var k = 0; k < buttonElems.length; k++) {
+                    
+                        listOfElements.push(buttonElems[k])
+                }
                 break;
 
             case "all":
-                var elems = document.body.getElementsByTagName("*");
+                listOfElements = document.body.getElementsByTagName("*");
                 break;
 
             case "select":
-                var elems = document.body.getElementsByTagName("select");
+                listOfElements = document.body.getElementsByTagName("select");
+                break;
+
+            case "link":
+                listOfElements = document.body.getElementsByTagName("a");
                 break;
 
             default:
-                var elems = document.body.getElementsByTagName(elemType);
+                listOfElements = document.body.getElementsByTagName(elemType);
                 break;
         }
 
@@ -35,7 +55,7 @@
         var totalUniqueStyle = 0;
         var listOfUniqueStyles = [];
 
-        getFontsList(elems, uniqueStyleList);
+        getFontsList(listOfElements, uniqueStyleList, elemType);
         displayInConsole();
 
     }
@@ -45,7 +65,10 @@
         if (!console) {
             alert("Please open Developer Toolbar before using this tool!");
         }
-
+        if(listOfUniqueStyles["ALLFIORI"]){
+                console.log("All "+elemType+" match FIORI style");
+                return;
+        }
 
         if (listOfUniqueStyles.length > 0) {
             console.log("Total distinct " + propertyType + " used : " + totalUniqueStyle);
@@ -57,31 +80,24 @@
 
     }
 
-    function getFontsList(elems, uniqueStyleList) {
+    function getFontsList(elems, uniqueStyleList, elemType) {
+        
         for (var k = 0; k < elems.length; k++) {
 
             if (elems[k]) {
-                var f;
-                if (!inputField) {
-                    f = document.defaultView.getComputedStyle(elems[k], null).getPropertyValue(propertyType);
-                } else {
-                    if (elems[k].type == 'text' || elems[k].type == 'button' || elems[k].type == 'submit') {
-                        f = document.defaultView.getComputedStyle(elems[k], null).getPropertyValue(propertyType);
-                    }
-                }
+                var f= document.defaultView.getComputedStyle(elems[k], null).getPropertyValue(propertyType);                                            
+                    if(elems[k].type !== "hidden"){
 
-
-                if (uniqueStyleList[f]) {
-                    var tempArr = uniqueStyleList[f];
-                    tempArr.push(elems[k]);
-                    uniqueStyleList[f] = tempArr;
-                } else {
-                    uniqueStyleList[f] = [elems[k]];
-                    listOfUniqueStyles.push(f);
-                    totalUniqueStyle++;
-                }
-
-
+                        if (uniqueStyleList[f]) {
+                            var tempArr = uniqueStyleList[f];
+                            tempArr.push(elems[k]);
+                            uniqueStyleList[f] = tempArr;
+                        } else {
+                            uniqueStyleList[f] = [elems[k]];
+                            listOfUniqueStyles.push(f);
+                            totalUniqueStyle++;
+                        }
+                    }                             
             }
 
         }
